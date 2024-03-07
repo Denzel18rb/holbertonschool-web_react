3579f72a47e2ccca4b4a71d536f6f21596e2aabf
Denzel18rb <74129875rb@gmail.com>
2024-03-07 15:20:45 -0500

    Add main.ts task_1/js

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,54 @@
+// Definition of the 'Teacher' interface
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: string | boolean | number | undefined; // To allow any attribute
+  }
+  
+  // Definition of the 'Directors' interface that extends 'Teacher'
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  // Definition of the 'printTeacherFunction' interface
+  interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  // Definition of the 'Student' interface
+  interface Student {
+    firstName: string;
+    lastName: string;
+  }
+  
+  // Definition of the 'StudentClass' interface
+  interface StudentClassInterface {
+    new (firstName: string, lastName: string): StudentClass;
+  }
+  
+  // Definition of the 'StudentClass' class
+  class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+  
+  // Example of use with 'StudentClass'
+  const student1: StudentClass = new StudentClass('Miguel', 'Bautista');
+  console.log(student1.workOnHomework()); // Output: Currently working
+  console.log(student1.displayName()); // Output: Alice
