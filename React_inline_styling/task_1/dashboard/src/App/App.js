a3d073243ae8da0f37b228cd582ce3fed9106d55
Denzel18rb <74129875rb@gmail.com>
2024-03-28 12:23:28 -0500

    updated

--- React_inline_styling/task_1/dashboard/src/App/App.js
+++ React_inline_styling/task_1/dashboard/src/App/App.js
@@ -1,155 +1,106 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import Header from '../Header/Header.js';
-import Login from '../Login/Login.js';
-import Footer from '../Footer/Footer.js';
-import Notifications from '../Notifications/Notifications.js';
+import React from 'react';
+import Header from '../Header/Header';
+import Notifications from '../Notifications/Notifications';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom.js';
-import BodySection from '../BodySection/BodySection.js';
-import WithLogging from '../HOC/WithLogging.js';
 import { StyleSheet, css } from 'aphrodite';
 
-class App extends Component {
+const styles = StyleSheet.create({
+  app: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr auto',
+    minHeight: '100vh',
+    width: '100%',
+  },
+  body: {
+    borderTop: '4px solid #e0354b',
+    borderBottom: '4px solid #e0354b',
+    padding: '2rem',
+  },
+  footer: {
+    width: '100%',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    fontSize: '1.25rem',
+  },
+});
+
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.ctrlHEventHandler = this.ctrlHEventHandler.bind(this);
+    this.state = {
+      listCourses: [
+        { id: 1, name: 'ES6', credit: 60 },
+        { id: 2, name: 'Webpack', credit: 20 },
+        { id: 3, name: 'React', credit: 40 },
+      ],
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+      ],
+    };
   }
 
-  ctrlHEventHandler(e) {
-    let k = e.key;
-    if ((e.metaKey || e.ctrlKey) && k === 'h') {
-      e.preventDefault();
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
       alert('Logging you out');
       this.props.logOut();
     }
   };
 
-  handleKeyPressDown() {
-    document.addEventListener("keydown", this.ctrlHEventHandler, false);
-  };
-
   componentDidMount() {
-    this.handleKeyPressDown();
-  };
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.ctrlHEventHandler, false);
-  };
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
 
   render() {
-
-    let {
-      isLoggedIn,
-    } = this.props;
-
-    let i = 0;
-    
-    let listNotifications = [
-      {
-        id: i++,
-        type: "default",
-        value: "New course available",
-      },
-      {
-        id: i++,
-        type: "urgent",
-        value: "New resume available",
-      },
-      {
-        id: i++,
-        type: "urgent",
-        html: {__html: getLatestNotification()},
-      }
-    ];
-
-    let listCourses = [
-      {
-        id: 1,
-        name: "ES6",
-        credit: 60,
-      },
-      {
-        id: 2,
-        name: "Webpack",
-        credit: 20,
-      },
-      {
-        id: 3,
-        name: "React",
-        credit: 40,
-      },
-    ];
+    const { isLoggedIn } = this.props;
 
     return (
-      <Fragment>
+      <>
+        <Notifications listNotifications={this.state.listNotifications} />
         <div className={css(styles.app)}>
-          <div className={css(styles.upperside)}>
-            <Notifications listNotifications={listNotifications} />
-            <Header />
+          <Header />
+          <div className={css(styles.body)}>
+            {isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={this.state.listCourses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login />
+              </BodySectionWithMarginBottom>
+            )}
+            {
+              <BodySection title="News from the School">
+                <p>More news soon...</p>
+              </BodySection>
+            }
           </div>
-          {
-            isLoggedIn === false &&
-            <BodySectionWithMarginBottom title="Log in to continue">
-              <Login />
-            </BodySectionWithMarginBottom>
-          }
-          {
-            isLoggedIn === true &&
-            <BodySectionWithMarginBottom title="Course list">
-              <CourseList listCourses={listCourses} />
-            </BodySectionWithMarginBottom>
-          }
-          <BodySection title="News from the school">
-            <p>
-              Ipsum anim sunt qui ullamco do consequat reprehenderit
-              aliqua fugiat proident amet duis.
-            </p>
-          </BodySection>
-          <Footer />
+          <div className={css(styles.footer)}>
+            <Footer />
+          </div>
         </div>
-      </Fragment>
-    );  
-  };
-};
-
-const styles = StyleSheet.create({
-  app: {
-    position: 'relative',
-    minHeight: '100vh',
-  },
-  upperside: {
-    display: "flex",
-    flexDirection: "row-reverse",
-    width: "100%",
-    borderBottom: `3px solid var(--holberton-red)`,
-    justifyContent: "space-between",
+      </>
+    );
   }
-});
+}
 
-// const globalStyles = StyleSheet.create({
-//   globals: {
-//     ':root': {
-//       '--holberton-red': '#e1484c',
-//     },
-//     'body': {
-//       maxWidth: '90%',
-//       margin: '0 auto',
-//     },
-//     'div': {
-//       padding: '2px 8px',
-//     },
-//   },
-// });
-
-// css(globalStyles.globals);
-
-App.propTypes = {
+Notifications.propTypes = {
+  isLoggedIn: PropTypes.bool,
   logOut: PropTypes.func,
 };
 
-App.defaultProps = {
+Notifications.defaultProps = {
   isLoggedIn: false,
   logOut: () => {},
 };
